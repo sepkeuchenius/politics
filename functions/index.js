17fcf9b2e82009ea32513ef76e0cdf5b74230ea8
Sep Keuchenius <sep@sep.dev>
2023-08-13 12:34:35 +0200

    removed unused functions

--- functions/index.js
+++ functions/index.js
@@ -3,18 +3,6 @@ const algoliasearch = require("algoliasearch");
 const client = algoliasearch('PD7R6XVZ97', process.env.ALGOLIA_API_KEY);
 const index = client.initIndex('parties');
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
-
-exports.addMessage = functions.https.onCall((data, context) => {
-  return "Ot";
-});
-
 exports.search  = functions.runWith({secrets:["ALGOLIA_API_KEY"]}).https.onCall(async (data, context) => {
   return await index.search(data.query).then((res)=>{
     return generate_graph_from_hits(res.hits);
