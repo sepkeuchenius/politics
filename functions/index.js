def03d75880576a7f5ff46886348d7cec75c7722
Sep Keuchenius <sep@sep.dev>
2023-08-21 22:56:05 +0200

    get motions as well

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions");
 const algoliasearch = require("algoliasearch");
+const { object } = require("firebase-functions/v1/storage");
 const client = algoliasearch('PD7R6XVZ97', process.env.ALGOLIA_API_KEY);
 const index = client.initIndex('parties');
 
@@ -11,10 +12,14 @@ exports.search  = functions.runWith({secrets:["ALGOLIA_API_KEY"]}).https.onCall(
 
 
 function _get_hit_parties(hits){
-  return hits.map(function(hit){return hit.party}).filter(function(party, index, parties){return parties.indexOf(party) == index})
+  return hits.map(function(hit){return hit.party}).filter(function(party, index, parties){return party && parties.indexOf(party) == index})
 }
 
-function _generate_party_node(party, index){
+function _get_hit_members(hits){
+  return hits.map(function(hit){return hit.member}).filter(function(member, index, members){return member && members.indexOf(member) == index})
+}
+
+function _generate_root_node(party, index){
   return {
     "id": index,
     "label": party,
@@ -38,14 +43,20 @@ function generate_graph_from_hits(hits){
   var nodes = []
   var edges = []
   var hits = hits.filter(_find_highlight)
-  var parties = _get_hit_parties(hits)
-  var party_nodes = parties.map(_generate_party_node)
-  nodes = nodes.concat(party_nodes)
+  var roots = _get_hit_parties(hits).concat(_get_hit_members(hits))
+  var root_nodes = roots.map(_generate_root_node)
+  nodes = nodes.concat(root_nodes)
   for(hit of hits){
     hit.text = _clean_text(hit.text)
     node = _generate_node(hit)
     nodes.push(node)
-    edge = {"from": hit.objectID, "to": parties.indexOf(hit.party)}
+    if (hit.hasOwnProperty("party")){
+      edge = {"from": hit.objectID, "to": roots.indexOf(hit.party)}
+    }
+    else {
+      edge = {"from": hit.objectID, "to": roots.indexOf(hit.member)}
+
+    }
     edges.push(edge)
   }
   return [nodes, edges]
