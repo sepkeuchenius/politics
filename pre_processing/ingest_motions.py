1dacf0c37203b547182761a529206e40bbdf5378
Sep Keuchenius <sep@sep.dev>
2023-09-30 15:11:33 +0200

    ingesting motions in btach

--- pre_processing/ingest_motions.py
+++ pre_processing/ingest_motions.py
@@ -1,8 +1,15 @@
-from utils import COLLECTION
+from utils import COLLECTION, FIRESTORE_DB
 from os import listdir
 import json
 
-for filename in listdir("data/motions"):
-    with open(f"data/motions/{filename}") as file:
+batch = FIRESTORE_DB.batch()
+print("started batch")
+
+for filename in listdir("pre_processing/data/motions"):
+    with open(f"pre_processing/data/motions/{filename}") as file:
         motion:dict = json.load(file)
-        COLLECTION.document(motion.get('Id')).set(motion)
+        doc = COLLECTION.document(motion.get('Id'))
+        print("added doc to batch")
+        batch.set(doc, motion)
+
+batch.commit()
